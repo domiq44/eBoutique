1cd61f6a2f369fd527774d0664b055710274d7a9
domiq44 <dc.geslain@free.fr>
2018-11-15 05:16:15 +0100

    Bow use appliation.properties

--- src/main/java/fr/domiq/config/DataSourceConfig.java
+++ src/main/java/fr/domiq/config/DataSourceConfig.java
@@ -2,16 +2,23 @@ package fr.domiq.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@PropertySource(value = { "classpath:application.properties" })
 @ComponentScan(basePackages = "fr.domiq")
 public class DataSourceConfig {
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public DataSource dataSource() {
@@ -20,10 +27,10 @@ public class DataSourceConfig {
 
 	private DataSource dataSourceH2() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:file:./test");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
 		return dataSource;
 	}
 
